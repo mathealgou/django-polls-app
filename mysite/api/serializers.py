e08e21250c33753e56b339dedb092597d00985f6
Matheus <matheus.goulart@penseavanti.com.br>
2022-08-14 11:50:01 -0300

    Choice api routes added.

--- mysite/api/serializers.py
+++ mysite/api/serializers.py
@@ -1,7 +1,16 @@
-from polls.models import Question
+from polls.models import Question, Choice
 from rest_framework import serializers
 
+
 class QuestionSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Question
-		fields = ('id', 'question_text', 'pub_date')
+    class Meta:
+        model = Question
+        fields = '__all__'
+        
+        # fields = ('id', 'question_text', 'pub_date')
+  
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = '__all__'
+        # fields = ('id', 'question', 'choice_text', 'votes')
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from polls.models import Question
-from .serializers import QuestionSerializer
+from polls.models import Question, Choice
+from .serializers import QuestionSerializer, ChoiceSerializer
 from rest_framework import viewsets
 
 # Create your views here.
 class QuestionViewSet(viewsets.ModelViewSet):
-	queryset = Question.objects.all()
-	serializer_class = QuestionSerializer
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+ 
+class ChoiceViewSet(viewsets.ModelViewSet):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
     
